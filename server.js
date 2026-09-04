45ed7c5c56e5ebd4c9394779f36a45a9c15e3ec9
JCMaryniuk <josiah.thp@gmail.com>
2026-09-03 23:07:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import OpenAI from "openai";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
