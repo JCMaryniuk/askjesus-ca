a69e642f1ad3467243f7db967a610ffcc44cb669
JCMaryniuk <josiah.thp@gmail.com>
2026-09-03 22:46:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from "express";
-import cors from "cors";
 import OpenAI from "openai";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
